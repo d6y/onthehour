f0a676dffca2f97098018b8cffbaa7f812516327
Richard Dallaway <richard@dallaway.com>
2018-05-25 22:36:42 +0100

    read credentials from env, and tweet

--- README.md
+++ README.md
@@ -9,4 +9,24 @@ And push the binary out:
 
 ```
 scp target/x86_64-unknown-linux-musl/release/onthehour server.someplace:
+```
 
+# How to create Twitter credentials
+
+Go to https://apps.twitter.com/ and "Create a new App".
+Once that is done, in the "Keys and Access Tokens" tab, 
+make a note of:
+
+- Consumer Key (API Key)
+- Consumer Secret (API Secret)
+
+Scroll down and press "Create my access token", and then note:
+
+- Access Token
+- Access Token Secret
+
+When you run `onthehour` do so with the following environment variables...
+
+```
+CONSUMER_KEY=??? CONSUMER_SECRET=??? ACCESS_TOKEN=??? ACCESS_SECRET=??? onthehour
+```
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,50 @@
 extern crate chrono;
 extern crate chrono_tz;
+extern crate tweetust;
+
+#[macro_use]
+extern crate serde_derive;
+extern crate envy;
 
 use chrono::{DateTime, Utc, Timelike, TimeZone};
 use chrono_tz::Tz;
 use chrono_tz::Europe::London;
+use tweetust::*;
+use tweetust::models::*;
+
+#[derive(Deserialize, Debug)]
+struct TwitterCredentials {
+    consumer_key: String,
+    consumer_secret: String,
+    access_token: String,
+    access_secret: String,
+}
 
 fn main() {
     let now: DateTime<Utc> = Utc::now();
     let msg = tweet_for(now);
-    println!("{}", msg);
+
+    match read_credentials() {
+        Err(e) => eprintln!("Would have sent {}, but won't because {}", msg, e),
+        Ok(creds) => {
+            let result = send_tweet(creds, &msg);
+            println!("{:?}", result);
+        }
+    }
+}
+
+fn read_credentials() -> Result<TwitterCredentials, envy::Error> {
+    envy::from_env::<TwitterCredentials>()
+}
+
+fn send_tweet(creds: TwitterCredentials, msg: &str) -> Result<TwitterResponse<Tweet>, TwitterError> {
+    let handler = DefaultHttpHandler::with_https_connector().unwrap();
+    let auth = OAuthAuthenticator::new(creds.consumer_key, creds.consumer_secret, creds.access_token, creds.access_secret);
+
+    TwitterClient::new(auth, handler)
+        .statuses()
+        .update(msg)
+        .execute()
 }
 
 fn tweet_for(when: DateTime<Utc>) -> String {
@@ -43,6 +79,7 @@ mod tests {
         let morning = Utc.ymd(2018, 1, 2).and_hms(9, 8, 7);
         assert_eq!(tweet_for(morning), "It's 9 o'clock (09:00)");
     }
+
     #[test]
     fn handles_12hour_time_bst() {
         let morning = Utc.ymd(2018, 5, 2).and_hms(8, 8, 7);
