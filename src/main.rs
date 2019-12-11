0c340a30c895531767cee1de4c351b0d095b1f39
Richard Dallaway <richard@dallaway.com>
2019-12-11 13:36:19 +0000

    'it's 1 o'clock again' for summer to utc

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ extern crate openssl_probe;
 extern crate serde_derive;
 extern crate tweetust;
 
-use chrono::{DateTime, TimeZone, Timelike, Utc};
+use chrono::{DateTime, Duration, TimeZone, Timelike, Utc};
 use chrono_tz::Europe::London;
 use chrono_tz::Tz;
 use serde_derive::Deserialize;
@@ -70,9 +70,23 @@ fn send_tweet(
 fn tweet_for(when: DateTime<Utc>) -> String {
     let uk: DateTime<Tz> = London.from_utc_datetime(&when.naive_utc());
     match uk.time().hour() {
+        // Custom text for 00:00:
         0 => "It's midnight".to_string(),
+
+        // Summer time to UTC hour repeat:
+        n if n == an_hour_back(&when) => uk.format("It's %-l o'clock again (%H:00)").to_string(),
+
+        // Common case:
         _ => uk.format("It's %-l o'clock (%H:00)").to_string(),
     }
+}
+
+fn an_hour_back(when: &DateTime<Utc>) -> u32 {
+    let previous_hour_utc = *when - Duration::hours(1);
+    London
+        .from_utc_datetime(&previous_hour_utc.naive_utc())
+        .time()
+        .hour()
 }
 
 #[cfg(test)]
@@ -103,5 +117,22 @@ mod tests {
     fn handles_12hour_time_bst() {
         let morning = Utc.ymd(2018, 5, 2).and_hms(8, 8, 7);
         assert_eq!(tweet_for(morning), "It's 9 o'clock (09:00)");
+    }
+
+    #[test]
+    fn handles_repeated_hour_when_clocks_change() {
+        // Clocks went back on 27 Oct 2019 at 2am summer time, which is 1am UTC
+
+        // So UTC midnight is 1am summer time:
+        let clock_goes_back = Utc.ymd(2019, 10, 27).and_hms(0, 0, 0);
+        assert_eq!(tweet_for(clock_goes_back), "It's 1 o'clock (01:00)");
+
+        // ...and an hour later it's 1am again:
+        let an_hour_later = clock_goes_back + Duration::hours(1);
+        assert_eq!(tweet_for(an_hour_later), "It's 1 o'clock again (01:00)");
+
+        // Another hour on and it's 2am:
+        let two_hours_later = an_hour_later + Duration::hours(1);
+        assert_eq!(tweet_for(two_hours_later), "It's 2 o'clock (02:00)");
     }
 }
